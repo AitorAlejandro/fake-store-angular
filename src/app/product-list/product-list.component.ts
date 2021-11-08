4a74e4e4e27247132f71b84ecb34f1ef72e79eb6
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-09 00:42:42 +0100

    create Product type

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../core/entities';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
   title = 'Product List';
 
-  constructor() { }
+  product: Product = {
+    category: 'category',
+    description: 'description',
+    id: 1,
+    image: '',
+    price: 0,
+    rating: {
+      count: 1,
+      rate: 1,
+    },
+    title: 'title',
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
