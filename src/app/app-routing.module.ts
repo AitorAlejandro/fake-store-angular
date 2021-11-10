25bd249ec53e832a9426880aa268f7ff8b3c5d09
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-10 21:35:15 +0100

    wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProductListComponent} from "./components/product-list/product-list.component"
 
 const routes: Routes = [
-  { path: '', component: ProductListComponent },
+  { path: '**', component: ProductListComponent },
 ];
 
 @NgModule({
